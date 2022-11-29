0ba3799e75daed4530de7327b548973687da6759
Wilkson Pedro da Silva <wil.thydus@gmail.com>
2022-11-29 19:42:22 -0300

    exercicios de logica de programação

---
+++ Fundametos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/Array-e-Loop-for/exemplosDeArrays.js
@@ -0,0 +1,59 @@
+//let number =[0,1,2,3,4,5,6,7,8,9,10];
+
+/*//chamando elementos do array
+console.log(number[0]);
+//tamanho do array 
+console.log(number.length);
+//pegar o ultimo item do array
+console.log(number[number.length -1]);
+//laço com repetição do array (percorre cada elemento do array)
+for (let index = 0; index < number.length; index += 1);*/
+
+/*----------------------------------------------------------------------------------------------------------------
+1. Encontre o index através do valor de um elemento
+Escreva um algoritmo que recebe 2 parâmetros:
+
+Um array contendo um conjunto de números
+O valor de um desses elementos
+Procure esse elemento dentro do array, e imprima no terminal a posição index em que o elemento encontra-se.
+
+Caso não o encontre, imprima:
+
+"Elemento não encontrado no array"
+*/
+/*let escolher = 9;
+let resultado = 0;
+for (let index =0; index < number.length; index +=1){
+    if (escolher === number[index]){
+    resultado += number[index];
+
+    } else{
+        console.log('elemento não encontrado');
+    }
+}
+console.log(resultado);*/
+
+/*---------------------------------------------------------------------------------------------------------------------------
+2. Retorne os números ímpares
+A Dora aventureira gostaria de ter uma lista com todos o números ímpares entre 1 e 50.
+
+Escreva um algoritmo que imprima no terminal uma string com todos os números inteiros ímpares desse intervalo.
+
+Use a seguinte estrutura na impressão:
+-------------------------------------------------------------------------------------------------------------------
+let resu = [];
+for  (let index = 1; index <= 50; index += 2){
+    resu.push(index);
+}
+console.log(resu);
+-------------------------------------------------------------------------------------------------------------------*/
+
+/*
+let imp = [];
+for (let index = 0; index <= 50; index +=1){
+    if (index %2 !== 0){
+        imp.push(index);
+    }
+}
+console.log(imp);
+*/
