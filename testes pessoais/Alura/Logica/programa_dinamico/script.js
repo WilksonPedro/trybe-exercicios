3f07ef9c2715682fb97f4fb3d71190b2f1598dd9
Wilkson Pedro da Silva <wil.thydus@gmail.com>
2023-03-19 20:05:51 -0300

    exercicio de logica

---
+++ testes pessoais/Alura/Logica/programa_dinamico/script.js
@@ -0,0 +1,26 @@
+/*var idadeFlavio = 39;
+var idadeJoaquim = 20;
+var idadeBarney = 41;
+
+var media = (idadeFlavio + idadeJoaquim + idadeBarney)/3;
+
+document.write("A média das idades é " + Math.round(media));
+
+var nome = "Flávio";
+
+document.write("<br>");
+document.write("A idade de " + nome + " é " + idadeFlavio);*/
+/*
+document.write('<h3>Álcool ou Gasolina?</h3>');
+var tanque = 40;
+
+var caminhoComGasolina = 480;
+var consumoDeGasolina = caminhoComGasolina/tanque;
+
+var caminhoComAlcool = 300;
+var consumoDeAlcool = caminhoComAlcool/tanque;
+
+document.write("O consumo de Gasolina é " + consumoDeGasolina + " km/L");
+document.write('<br>');
+document.write("O consumo de Álcool é " + consumoDeAlcool + " km/L");
+*/
