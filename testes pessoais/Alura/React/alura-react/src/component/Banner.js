1793d976f2c4b814a99c3957281503cc79930189
Wilkson Pedro da Silva <wil.thydus@gmail.com>
2023-03-27 18:04:36 -0300

    Primeira parte do organo Alura

--- testes pessoais/Alura/React/alura-react/src/component/Banner.js
+++ testes pessoais/Alura/React/alura-react/src/component/Banner.js
@@ -1,5 +1,12 @@
-function Banner() {
-    <img src="imagens/banner.png" alt="banner do cusrso de react" />
-}
+import React, { Component } from 'react'
+import './Banner.css'
 
-export default Banner
+export default class Banner extends Component { // podemos deixar o export aqui em cima em vez do final da pagina.
+  render() {
+    return (
+        <header className='banner'>
+            <img src='./imagens/banner.png' alt='imagem do banner principal' />
+        </header>
+    )
+  }
+}
