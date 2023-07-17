7f7aad0ad8f78aacc8afe621ed68267c413d8e57
Wilkson Pedro da Silva <wil.thydus@gmail.com>
2023-07-17 17:56:58 -0300

    Primeiro Componente criado

---
+++ Front-end/02-introdução ao react/dia-01-hello-world-no-react/primeiro-contato-react/primeiro-contato-react/src/HelloWolrd.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HelloWorld() {
+    return (
+        <>
+        <span>Hello Wolrd, estou aqui!</span>
+        <h1>Vamos aprender de verdade dessa!</h1>
+        </>
+    )
+}
+
+export default HelloWorld;
