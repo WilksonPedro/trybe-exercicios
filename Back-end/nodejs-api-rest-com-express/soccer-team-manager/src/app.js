b403889fc1400dde2de910098c8c9ca06878f3ba
Wilkson Pedro da Silva <wil.thydus@gmail.com>
2023-09-11 16:46:50 -0300

    criado o primeiro servidor node

---
+++ Back-end/nodejs-api-rest-com-express/soccer-team-manager/src/app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+// importação do express que serve para criar o servidor
+const app = express();
+
+module.exports = app;
+// exportação do app para ser usado em outros arquivos
---
+++ Back-end/nodejs-api-rest-com-express/soccer-team-manager/src/server.js
@@ -0,0 +1,5 @@
+// src/server.js
+const app = require('./app');
+
+app.listen(3001, () => console.log('servidor online na porta 3001'));
+// o listen é uma função do express para rodar o servidor na porta 3001
