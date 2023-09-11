3564e1eb9b747f3982b72d0cc08da67d8b76379a
Wilkson Pedro da Silva <wil.thydus@gmail.com>
2023-09-11 18:16:52 -0300

    adicionamdo priemiro comando  node

--- Back-end/nodejs-api-rest-com-express/soccer-team-manager/src/app.js
+++ Back-end/nodejs-api-rest-com-express/soccer-team-manager/src/app.js
@@ -2,5 +2,8 @@ const express = require('express');
 // importação do express que serve para criar o servidor
 const app = express();
 
+app.get('/', (req, res) =>
+res.status(200).json({ message: 'Ola mundo versão node!' }));
+
 module.exports = app;
 // exportação do app para ser usado em outros arquivos
