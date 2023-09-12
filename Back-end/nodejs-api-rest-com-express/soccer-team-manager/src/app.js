709c164cc58749407ef973f310400974187a3a77
Wilkson Pedro da Silva <wil.thydus@gmail.com>
2023-09-12 14:56:09 -0300

    adcionado comentario de explicação

--- Back-end/nodejs-api-rest-com-express/soccer-team-manager/src/app.js
+++ Back-end/nodejs-api-rest-com-express/soccer-team-manager/src/app.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.get('/', (req, res) =>
 res.status(200).json({ message: 'Ola mundo versão node!' }));
+// '/' é a rota raiz do servidor qualquer coisa depois da barra são rotas secundarias
 
 module.exports = app;
 // exportação do app para ser usado em outros arquivos
