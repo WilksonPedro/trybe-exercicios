32e34b39f32ded9619d9e14323a35297d9e26eba
Wilkson Pedro da Silva <wil.thydus@gmail.com>
2023-09-28 15:34:48 -0300

    usando uma req e mais exemplos

--- Back-end/nodejs-api-rest-com-express/soccer-team-manager/src/app.js
+++ Back-end/nodejs-api-rest-com-express/soccer-team-manager/src/app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 // importação do express que serve para criar o servidor
 const app = express();
-
+// criação do servidor express e atribuição a variavel app
+app.use(express.json());
+// o app.use deve ser colocado para a leitura do .json
 app.get('/', (req, res) =>
+// '/' é a rota principal tmb chamada raiz outra poderia ser '/home', '/login' etc
 res.status(200).json({ message: 'Ola mundo versão node!' }));
+/*
+O get serve para fazer uma requisição que retorna um json com a mensagem 
+"Ola mundo versão node!" e o status 200 que significa que a requisição foi bem sucedida
+*/
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.status(200).json({ message: 'alguma resposta' });
+});
 
 module.exports = app;
 // exportação do app para ser usado em outros arquivos
