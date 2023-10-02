daf57bb8c381e179446aa6e72f9c371e19fe164a
Wilkson Pedro da Silva <wil.thydus@gmail.com>
2023-10-02 15:25:18 -0300

    exercicio de api

--- Back-end/nodejs-api-rest-com-express/soccer-team-manager/src/app.js
+++ Back-end/nodejs-api-rest-com-express/soccer-team-manager/src/app.js
@@ -1,3 +1,5 @@
+/*
+
 const express = require('express');
 // importação do express que serve para criar o servidor
 const app = express();
@@ -10,7 +12,7 @@ res.status(200).json({ message: 'Ola mundo versão node!' }));
 /*
 O get serve para fazer uma requisição que retorna um json com a mensagem 
 "Ola mundo versão node!" e o status 200 que significa que a requisição foi bem sucedida
-*/
+*//*
 app.post('/', (req, res) => {
     console.log(req.body);
     res.status(200).json({ message: 'alguma resposta' });
@@ -18,3 +20,36 @@ app.post('/', (req, res) => {
 
 module.exports = app;
 // exportação do app para ser usado em outros arquivos
+*/
+
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const teams = [
+    {
+        id: 1,
+        name: 'sao paulo',
+        iniciais: 'sp',
+    },
+    {
+        id: 2,
+        name: 'palemiras',
+        iniciais: 'pal',
+    },
+];
+
+app.get('/', (req, res) => 
+res.status(200).json({ message: 'Ola mundo versão node!' }));
+
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
+
+app.post('/teams', (req, res) => {
+    const newTeam = { ...req.body };
+    teams.push(newTeam);
+
+    res.status(201).json({ team: newTeam });
+});
+
+module.exports = app;
